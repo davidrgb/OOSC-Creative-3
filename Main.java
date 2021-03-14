30e1d452cdd4eefbeef176ffae810ac1d0044e1e
davidrgb <davidrussellrgb@gmail.com>
2021-03-14 01:51:17 -0600

    Centered window on start

--- Main.java
+++ Main.java
@@ -1,13 +1,24 @@
 import javax.swing.JFrame;
 
+import java.awt.Dimension;
+import java.awt.Toolkit;
+
 import view.GamePanel;
 
 public class Main {
     public static void main(String[] args) {
         JFrame window = new JFrame();
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        window.setLocation(500, 200);
         window.setTitle("Blackjack");
+
+        Dimension resolution = Toolkit.getDefaultToolkit().getScreenSize();
+        final int SCREEN_X_RESOLUTION = resolution.width;
+        final int SCREEN_Y_RESOLUTION = resolution.height;
+
+        final int X_RESOLUTION = 1000;
+        final int Y_RESOLUTION = 450;
+
+        window.setLocation(SCREEN_X_RESOLUTION / 2 - (X_RESOLUTION / 2), SCREEN_Y_RESOLUTION / 2 - (Y_RESOLUTION / 2));
 
         var panel = new GamePanel(window);
         panel.init();
